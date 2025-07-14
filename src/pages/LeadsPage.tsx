882ef78b0d4ec491c476b93b02df7c716d18a9df
Theluxuryliving <theluxurylivingpk@gmail.com>
2025-07-15 00:26:11 +0500

    Update LeadsPage.tsx

--- src/pages/LeadsPage.tsx
+++ src/pages/LeadsPage.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AddLeadModal from "../components/AddLeadModal";
+
 
 export default function LeadsPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
