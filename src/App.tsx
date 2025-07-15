ca353f8707f50fa1807e5ec103248af1133c17ca
Atif Zubair <atif.zubair@gmail.com>
2025-07-15 18:28:56 +0500

    Fix: Use horizontal flex layout for sidebar + content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import LeadsPage from "./pages/LeadsPage";
+// import other pages as needed...
 
 export default function App() {
   return (
     <Router>
       <div className="flex font-poppins min-h-screen bg-[#f8f8f8]">
+        {/* Sidebar on the left */}
         <Sidebar />
-        <div className="flex-1 p-6">
+
+        {/* Main content on the right */}
+        <div className="flex-1 p-6 overflow-y-auto">
           <Routes>
             <Route path="/" element={<Navigate to="/leads" />} />
             <Route path="/leads" element={<LeadsPage />} />
+            {/* Add other routes here */}
           </Routes>
         </div>
       </div>
